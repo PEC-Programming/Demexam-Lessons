3e08610b24308af8c6d829f7497a7ae8a60795f6
mrnotbadguy <dmitrysafronov1996@yandex.ru>
2021-11-02 19:36:28 +0300

    Занятие 3 + задание

---
+++ занятие 3/SaloonSharm/Helpers/PageHelper.cs
@@ -0,0 +1,17 @@
+﻿using SaloonSharm.DbModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SaloonSharm.Helpers
+{
+    class PageHelper
+    {
+        public static Frame MainFrame;
+        public static TextBlock PageName;
+        public static SaloonDbEntities ConnectDb = new SaloonDbEntities();
+    }
+}
---
+++ занятие 3/SaloonSharm/Pages/SignInPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using SaloonSharm.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SaloonSharm.Pages
+{
+    /// <summary>
+    /// Interaction logic for SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : Page
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+
+            PageHelper.PageName.Text = "Вход в систему";
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbLogin.Text == null && pbPassword.Password == null)
+            {
+                MessageBox.Show("Не введены данные для входа", "Ошибка ввода");
+                return;
+            }
+
+            if (PageHelper.ConnectDb.Employee.Where(x => x.Login == tbLogin.Text).FirstOrDefault() == null)
+            {
+                MessageBox.Show("Пользователя с таким логином не существует", "Ошибка");
+                return;
+            }
+
+            if (PageHelper.ConnectDb.Employee.Where(x => x.Login == tbLogin.Text && x.Password == pbPassword.Password).FirstOrDefault() != null)
+            {
+                MessageBox.Show($"Добро пожаловать, {tbLogin.Text}", "Успех");
+                PageHelper.MainFrame.Navigate(new OrderPage());
+            }
+            else
+            {
+                MessageBox.Show("Введен неверный пароль", "Ошибка");
+                return;
+            }
+        }
+    }
+}
